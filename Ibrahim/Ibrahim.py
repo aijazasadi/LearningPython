1ea7f232c07abec06d8292466cab59893e5139e1
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-02 18:44:53 +0500

    Update Ibrahim.py

---
+++ Ibrahim/Ibrahim.py
@@ -0,0 +1,11 @@
+#This is a file where i will write my own code
+
+#decleration
+Welcome_message = "Welcome to Learning python on Visual studios "
+
+#input
+participant_1 = input("Enter your name: ")
+participant_2 = input("Enter your name: ")
+
+#output
+print(Welcome_message + participant_1 + " and " + participant_2)
