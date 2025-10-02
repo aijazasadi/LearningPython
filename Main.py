1e907e1b9213a9388443bb63f4d7308a9d9df4d3
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-02 11:11:55 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
-print("Welcome to Learning Python")
-print("My name is Muhammad Ibrahim")
-print("My name is Muhammad Annas")
+#decleration
+Welcome_message = "Welcome to Learning python on Vs "
+participant_1 = input("Enter your name: ")
+participant_2 = input("Enter your name: ")
+
+print(Welcome_message + participant_1 + " and " + participant_2)
