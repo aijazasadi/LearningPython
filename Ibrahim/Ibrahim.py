57eb8d46ee1605d4165d350678e16317f2a7e742
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-03 10:19:42 +0500

    Update Ibrahim.py

--- Ibrahim/Ibrahim.py
+++ Ibrahim/Ibrahim.py
@@ -6,9 +6,7 @@ Welcome_message = "Welcome to Learning python on Visual studios "
 #input
 participant_1 = input("Enter your name: ")
 participant_2 = input("Enter your name: ")
+teacher_1 = input("Enter your teacher's name: ")
 
 #output
-print(Welcome_message + participant_1 + " and " + participant_2)
-
-if participant_1 == "Ibrahim" or participant_2 == "Annas": 
-    print("your teacher is asad uncle")
+print(Welcome_message + participant_1 + " and " + participant_2 + " you are learning Python with " + teacher_1)
