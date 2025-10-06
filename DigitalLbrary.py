15e62769a13c0e130e7ed1080c9b41a2806da06e
Asad Aijaz Arain <aijaz.asadi@gmail.com>
2025-10-06 10:00:56 +0500

    DigitalLibrary.py and changes to main
    - Created DigitalLibrary.py
    - Modified Main.py learned assignment operator, multi-line comments. multi-var assign

---
+++ DigitalLbrary.py
@@ -0,0 +1,11 @@
+book_name
+#dd-mm-yyyy
+book_issue_date
+#dd-mm-yyyy
+book_return_date
+book_author
+book_genre
+book_publisher
+book_language
+# 1-4028-9462-7
+book_ISBN
--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
-#decleration
-Welcome_message = "Welcome to Learning python on Vs "
-participant_1 = input("Enter your name: ")
-participant_2 = input("Enter your name: ")
+"""
+This is a simple Python program that welcomes two participants to a learning session.
+"""
+#welcome message
+WELCOME_MESSAGE = "Welcome to Learning python on VsCode"
+#participant1 for the LibraryManagementSystem
+# participant_1 = input("Enter participiant1 : ")
+# #participant2 for the LibraryManagementSystem
+# participant_2 = input("Enter participiant2: ")
 
-print(Welcome_message + participant_1 + " and " + participant_2)
+# paricipiant_3, participiant_4 = "Asad" , "Fawad"
+
+# #legal variables
+
+
+# # print(WELCOME_MESSAGE + " " + participant_1 + " and " + participant_2)
+
+# print(WELCOME_MESSAGE , participant_1  , participant_2, paricipiant_3 , "and", participiant_4)
+
+print("Asad" == "Asad")
+#type casting
+print("Asad " + str(3))
