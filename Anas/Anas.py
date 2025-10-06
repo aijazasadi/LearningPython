64f76bc4ccca71c071faa30a01036db4abbfcaeb
TheOnlyGooseBall <annasrshd@gmail.com>
2025-10-06 10:29:37 +0500

    Update Anas.py

--- Anas/Anas.py
+++ Anas/Anas.py
@@ -1,7 +1,10 @@
-Welcome="Welcome to Learning python on Vs"
-print(Welcome)
+WELCOME="Welcome to Learning python on Vs"
+print(WELCOME)
 
 name1=input("Enter your name: ")
 name2=input("Enter your name: ")
 
-print(Welcome, name1 +" and "+ name2)
+print(WELCOME, name1 , " and " , name2)
+
+name1 = name2 
+print(name1)
