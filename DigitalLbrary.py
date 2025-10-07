9c36969450a54aff4d0c967257816a3dc15b88dc
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-07 10:48:44 +0500

    Format Digital library and Main.py
    Due to some previous deletions with both of these files i have reverted back some of the use full files

--- DigitalLbrary.py
+++ DigitalLbrary.py
@@ -1,18 +1,18 @@
-# #book_name
-# book_name = input("Enter book name: ")
+book_name
+#dd-mm-yyyy
+book_issue_date
+#dd-mm-yyyy
+book_return_date
+book_author
+book_genre
+book_publisher
+book_language
+# 1-4028-9462-7
+book_ISBN
 
-# #book_issue_date
-# book_issue_date = input("Enter book issue date (dd-mm-yyyy): ")
 
-# #book_author
-# book_author = input("Enter book author: ")
-
-# #book_ISBN
-# #1-4028-9462-7
-# book_ISBN = input("Enter book ISBN(#1-4028-9462-7): ")
 # list of book names
 book_names = ['Learn Python the Hard Way', 'Automate the Boring Stuff with Python', 'Python Crash Course', 'Fluent Python', 'Effective Python']
 book_issue_dates = ['01-01-2020', '15-03-2021', '22-07-2019', '10-10-2018', '05-05-2022']
 #output
-# print(book_name , "," , book_issue_date , "," , book_author , "," , book_ISBN)
 print("Available books in the library: ", *book_names, sep = ", ")
--- Main.py
+++ Main.py
@@ -1,5 +1,26 @@
-#decleration
-WELCOME_MESSAGE = "Welcome to Learning python on Vs-Code "
+#WELCOME_MESSAGE = "Welcome to Learning python on VsCode"
+#participant1 for the LibraryManagementSystem
+# participant_1 = input("Enter participiant1 : ")
+# #participant2 for the LibraryManagementSystem
+# participant_2 = input("Enter participiant2: ")
+
+
+#Welcome_message = "Welcome to Learning python on Vs "
+##participant_2 = input("Enter your name: ")
+
+# paricipiant_3, participiant_4 = "Asad" , "Fawad"
+# #legal variables
+
+# print(WELCOME_MESSAGE + " " + participant_1 + " and " + participant_2)
+# print(WELCOME_MESSAGE , participant_1  , participant_2, paricipiant_3 , "and", participiant_4)
+
+
+
+print("Asad" == "Asad")
+#type casting
+print("Asad " + str(3))
+print(Welcome_message + participant_1 + " and " + participant_2)
+
 #Multiline string
 MESSAGE = """This is a simple program to demonstrate input and output in Python.  
 You will be asked to enter your names."""
@@ -7,15 +28,9 @@ You will be asked to enter your names."""
 participant_1 = "Ibrahim"
 participant_2 = "Anas"
 
+#lists
 list_of_participants = [participant_1, participant_2, 1 , 23.4, True]
-"""
-(participants               )
-(marks               )
-(subjects               )
 
-"""
 list_of_participants = [[participant_1, participant_2 ] ,[44.5 , 52.4] ]
-# print(WELCOME_MESSAGE + participant_1 + " and " + participant_2)
-# print(MESSAGE)
 
 print(list_of_participants)
