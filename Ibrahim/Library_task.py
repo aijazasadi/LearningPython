6d64dceda9e3f73c4c7337719b60e944b318530e
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-07 10:30:01 +0500

    Create Library_task.py

---
+++ Ibrahim/Library_task.py
@@ -0,0 +1,15 @@
+#book_name
+book_name = input("Enter book name: ")
+
+#book_issue_date
+book_issue_date = input("Enter book issue date (dd-mm-yyyy): ")
+
+#book_author
+book_author = input("Enter book author: ")
+
+#book_ISBN
+#1-4028-9462-7
+book_ISBN = input("Enter book ISBN(#1-4028-9462-7): ")
+
+#output
+print(book_name , "," , book_issue_date , "," , book_author , "," , book_ISBN)
