2ce59ed0424e44d8880e9c6e54386fdb7d2530ec
Asad Aijaz Arain <aijaz.asadi@gmail.com>
2025-10-07 09:57:13 +0500

    list and multilist
    - Learned List
    - Learned Mult List

--- Main.py
+++ Main.py
@@ -1,6 +1,21 @@
 #decleration
-WELCOME_MESSAGE = "Welcome to Learning python on Vs "
-participant_1 = input("Enter your name participant1: ")
-participant_2 = input("Enter your name participant2: ")
+WELCOME_MESSAGE = "Welcome to Learning python on Vs-Code "
+#Multiline string
+MESSAGE = """This is a simple program to demonstrate input and output in Python.  
+You will be asked to enter your names."""
 
-print(WELCOME_MESSAGE + participant_1 + " and " + participant_2)
+participant_1 = "Ibrahim"
+participant_2 = "Anas"
+
+list_of_participants = [participant_1, participant_2, 1 , 23.4, True]
+"""
+(participants               )
+(marks               )
+(subjects               )
+
+"""
+list_of_participants = [[participant_1, participant_2 ] ,[44.5 , 52.4] ]
+# print(WELCOME_MESSAGE + participant_1 + " and " + participant_2)
+# print(MESSAGE)
+
+print(list_of_participants)
