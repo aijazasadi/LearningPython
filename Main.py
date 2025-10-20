fdd0bd5f4a7bc7ad07e0f191cdea457263726d7f
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-20 12:23:35 +0500

    Revert "string operation and a change in DigitalLibrary.py"
    This reverts commit 40c9ee8a7bd664bb25b08b2bbf122f4e0999d65d.

--- Main.py
+++ Main.py
@@ -33,23 +33,4 @@ list_of_participants = [participant_1, participant_2, 1 , 23.4, True]
 
 list_of_participants = [[participant_1, participant_2 ] ,[44.5 , 52.4] ]
 
-# print(list_of_participants)
-
-# for index in range(5):
-#     print("$" * int(index + 1))
-# print(len(participant_1))
-# print(participant_1[2:len(participant_1)])
-
-# if(participant_1[2:len(participant_1)] == "brahim"): #checking substring
-#     print("Yes")
-# else:
-#     print("No")
-
-# if(participant_1[2:3] in "brahim"): #in string function
-#     print("Yes")
-# else:
-#     print("No")
-
-# print("-".join(participant_1)) #joining string with separator
-# print(participant_1.swapcase()) #capitalizing first letter of string
-# print("-".join(reversed(participant_1)))
+print(list_of_participants)
