27889b913badb308f0907fce6d5a78c3133e78b3
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-10-25 09:52:13 +0500

    Update Book_Task.py

--- Ibrahim/Book_Task.py
+++ Ibrahim/Book_Task.py
@@ -7,11 +7,26 @@ book_name = ["Atomic Habits", "Breif history of Time", "Breif awnsers to big Que
 authors_name = ["James Clear", "Stephen Hawking", "Stephen Hawking", "Neil deGrasse Tyson", "Sun Tzu"]
 genres_names = ["Self-help", "Science", "Science", "Science", "Military Strategy"]
 
+all_details = [book_name, authors_name, genres_names]
+
 def print_book_details():
  if book_number >=0 and book_number < len(book_name):
     print (book_name[int(book_number)] , authors_name[int(book_number)] , genres_names[int(book_number)], sep= " / " )
  else:
     print("Invalid book number")
 
+def search_book(to_search, book_index):
+    for book in range(len(all_details)):
+        if to_search.lower() in all_details[book_index][book].lower():
+            return all_details
+    return None
+
+
 book_number = int(input("Which book details do you want to see?: "))
 print_book_details()
+
+search_input = input("Search book details:")  
+if search_book(search_input, book_index=0):
+    print("Found", search_input, "in the book details.")
+else: 
+    print(search_input, "not found in the book details.")
