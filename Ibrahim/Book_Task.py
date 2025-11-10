55f90529adbe53ca0b487ef09fd3c9e7ed6bb28c
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-11-10 08:29:46 +0500

    Cleanup

--- Ibrahim/Book_Task.py
+++ Ibrahim/Book_Task.py
@@ -1,12 +1,8 @@
-# Make a list of books, book authors, book genres. Say 5 books 
-# Print using print and sep syntax we learned
-# try fetching book 3 or 4 along with authors, genre and related information remember the first book __index stats with 0__ 
 
 #Default book lists
 book_name = ["Atomic Habits", "Breif history of Time", "Breif awnsers to big Questions", "Astrophysics for people in a Hurry", "Sun Zu Art of War"]
 authors_name = ["James Clear", "Stephen Hawking", "Stephen Hawking", "Neil deGrasse Tyson", "Sun Tzu"]
 genres_names = ["Self-help", "Science", "Science", "Science", "Military Strategy"]
-all_details = [book_name, authors_name, genres_names]
 
 #Add a book 
 def add_book():
@@ -29,9 +25,9 @@ def print_book_details():
 
 #search book by name
 def search_book(to_search, book_index):
-    for book in range(len(all_details)):
-        if to_search.strip().lower() in all_details[book_index][book].lower():
-            return all_details
+    for book in range(len(book_name)):
+        if to_search.strip().lower() in book_name[book_index][book].lower():
+            return book_name
     return None
 
 #input and search book by name
