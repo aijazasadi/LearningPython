aa44869be50b7ff40799c48cb981d1e0dbf30ed1
Asad Aijaz Arain <aijaz.asadi@gmail.com>
2025-11-12 18:26:01 +0500

    NamedTuple
    - Updated NamedTuple
     - added while loop Main.py

--- DigitalLbrary.py
+++ DigitalLbrary.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+from datetime import datetime
 #constants for book details indices
 BOOK_NAMES = 0
 ISSUE_DATES = 1
@@ -5,7 +7,6 @@ RETURN_DATES = 2
 AUTHORS = 3 
 
 # list of book names
-#length book
 book_names = [
     'Learn Python the Hard Way', # index 0
     'Automate the Boring Stuff with Python', #index 1
@@ -18,20 +19,30 @@ book_issue_dates = ['01-01-2020', '15-03-2021', '22-07-2019', '10-10-2018', '05-
 book_return_dates = ['01-02-2020', '15-04-2021', '22-08-2019', '10-11-2018', '05-06-2022', '12-01-2024']
 book_authors = ['Zed A. Shaw', 'Al Sweigart', 'Eric Matthes', 'Luciano Ramalho', 'Brett Slatkin', 'Zed A. Shaw']
 
-# books =[
-#     [book_names[0], book_issue_dates[0], book_return_dates[0], book_authors[0]], #[0][0]
-#     [book_names[1], book_issue_dates[1], book_return_dates[1], book_authors[1]], #
-#     [book_names[2], book_issue_dates[2], book_return_dates[2], book_authors[2]],
-#     [book_names[3], book_issue_dates[3], book_return_dates[3], book_authors[3]],
-#     [book_names[4], book_issue_dates[4], book_return_dates[4], book_authors[4]]
-# ]   
+# Example multiline book prefaces
+book_prefaces = [
+    """This book is a beginner's guide to learning Python programming.
+It provides step-by-step instructions and exercises to help you master the basics.""",
+    """This book teaches you how to automate everyday tasks using Python.
+It is perfect for office workers, students, and anyone who wants to save time.""",
+    """This book is a hands-on guide to learning Python programming.
+It includes practical projects to help you build real-world skills.""",
+    """This book dives deep into Python's advanced features.
+It is ideal for experienced programmers who want to write more efficient code.""",
+    """This book provides tips and tricks for writing clean and effective Python code.
+It is a must-read for Python developers who want to improve their skills."""
+]
+
 books = []
-for index, value in enumerate(book_names):
-    books.append([book_names[index], book_issue_dates[index], book_return_dates[index], book_authors[index]])
+
+book = namedtuple("Book", ['name', 'issue_date', 'return_date', 'author', 'book_preface'])
+
+zipped_books = zip(book_names, book_issue_dates, book_return_dates, book_authors, book_prefaces)
+
+books = [book(name, issue_date, return_date, author, book_preface) for name, issue_date, return_date, author, book_preface in zipped_books]
 books.sort()
 #function to print book details by index
 def print_book_details(book):
-    books.sort()
     if(book in books):
         print("Book Name: ", book[BOOK_NAMES])
         print("Book Issue Date: ", book[ISSUE_DATES])
@@ -42,7 +53,6 @@ def print_book_details(book):
         
 #printing all books
 def print_all_books():
-     books.sort()
      for book in books:
         print(f"Details of Book {book}:")
         print_book_details(book)
@@ -62,55 +72,39 @@ def get_books(to_search, book_index):
     return found_books
 
 def update_book(book, book_arg, new_change):
-    book[book_arg] = new_change
+    # replace will return a new namedtuple with the updated field
+    if(book_arg == AUTHORS):
+        updated_book = book._replace(author=new_change)
+    elif(book_arg == BOOK_NAMES):
+        updated_book = book._replace(name=new_change)
+    elif(book_arg == ISSUE_DATES):
+        updated_book = book._replace(issue_date=new_change)
+    elif(book_arg == RETURN_DATES):
+        updated_book = book._replace(return_date=new_change)
+    elif(book_arg == 4): # book_preface
+        updated_book = book._replace(book_preface=new_change)
+
+    book_index = books.index(book) #find index of the old book
+    books[book_index] = updated_book #replace with updated book
+    books.sort()
+
+def delete_book(book_to_delete): #remove book from the books list (books.pop() will remove the last book)
+    books.remove(book_to_delete)
     books.sort()
     
-def delete_book(bookToDelete): #remove book from the books list (books.pop() will remove the last book)
-    books.remove(bookToDelete)
-    books.sort()
 
 def add_book(new_book): #appends to the books list at the end
     books.append(new_book)
     books.sort()
 
 current_book = get_book("Brett   ", AUTHORS) # should return Zed A. Shaw
-# print_book_details(current_book)
 
-new_book = ["Deep Work", "12-12-2023", "12-01-2024", "Cal Newport"]
-
-# update_book(current_book, BOOK_NAMES, "Changed Book Title")
-
+new_book = book ("Deep Work", "12-12-2023", "12-01-2024", "Cal Newport", "Some preface about deep work.")
 add_book(new_book)
+# print(current_book)
 
-books = tuple(books)
-books = list(books)
-
-# add_book(["Python Tricks", "12-12-2023", "12-01-2024", "Dan Bader"])
-print(*books, sep="\n")
-# print(get_books("a. shaw", AUTHORS))
-
-# books = list(books_tuple)
-
-# print(type(books_tuple[2]))
-
-books[2][BOOK_NAMES] = "Modified Title"
-
-
-# print(books_tuple[2])
-# count = 0
-
-# while count < len(books_tuple):
-#     print(books_tuple[count])
-#     count += 1
-
-# for index, book in enumerate(books_tuple):
-#     print(f"Index {index}: {book}")
-
-# for index in range(len(books_tuple)):
-#     print(f"Index {index}: {books_tuple[index]}")   
-# print(*books, sep="\n")
-# returned_book = books[books.index(new_book)]
-# print(returned_book)
+delete_book(current_book)
+update_book(new_book, AUTHORS, "Asad Aijaz")
 
 
 
--- Main.py
+++ Main.py
@@ -1,60 +1,4 @@
 from collections import namedtuple
-# WELCOME_MESSAGE = "Welcome to Learning python on VsCode"
-# #participant1 for the LibraryManagementSystem
-# participant_1 = input("Enter participiant1 : ")
-# # #participant2 for the LibraryManagementSystem
-# participant_2 = input("Enter participiant2: ")
-
-
-# #Welcome_message = "Welcome to Learning python on Vs "
-# ##participant_2 = input("Enter your name: ")
-
-# # paricipiant_3, participiant_4 = "Asad" , "Fawad"
-# # #legal variables
-
-# # print(WELCOME_MESSAGE + " " + participant_1 + " and " + participant_2)
-# # print(WELCOME_MESSAGE , participant_1  , participant_2, paricipiant_3 , "and", participiant_4)
-
-
-
-# print("Asad" == "Asad")
-# #type casting
-# print("Asad " + str(3))
-# print(WELCOME_MESSAGE + participant_1 + " and " + participant_2)
-
-# #Multiline string
-# MESSAGE = """This is a simple program to demonstrate input and output in Python.  
-# You will be asked to enter your names."""
-
-# participant_1 = "Ibrahim"
-# participant_2 = "Anas"
-
-# #lists
-# list_of_participants = [participant_1, participant_2, 1 , 23.4, True]
-
-# list_of_participants = [[participant_1, participant_2 ] ,[44.5 , 52.4] ]
-
-# # print(list_of_participants)
-
-# # for index in range(5):
-# #     print("$" * int(index + 1))
-# # print(len(participant_1))
-# # print(participant_1[2:len(participant_1)])
-
-# # if(participant_1[2:len(participant_1)] == "brahim"): #checking substring
-# #     print("Yes")
-# # else:
-# #     print("No")
-
-# # if(participant_1[2:3] in "brahim"): #in string function
-# #     print("Yes")
-# # else:
-# #     print("No")
-
-# # print("-".join(participant_1)) #joining string with separator
-# # print(participant_1.swapcase()) #capitalizing first letter of string
-# # print("-".join(reversed(participant_1)))
-
 
 '''
 LISTS
@@ -105,3 +49,15 @@ fruit2 = Fruit("banana", 2.2, 20)
 
 print(f"{fruit1.name}, {fruit1.price}, {fruit1.quantity}")
 # print(fruit1)
+
+program_to_continue = True
+
+while(program_to_continue):
+    user_input = input("Enter fruit name to search (or 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        program_to_continue = False
+    else:
+        if user_input in fruits:
+            print(f"{user_input} is available.")
+        else:
+            print(f"{user_input} is not available.")
