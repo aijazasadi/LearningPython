ebaa16df713cb4004de91f3e67651933969f2d8d
ibrahimfawad26-stack <ibrahimfawad26@gmail.com>
2025-12-10 19:23:12 +0500

    Revision with Annas

---
+++ Ibrahim/Revision_Annas.py
@@ -0,0 +1,55 @@
+#termanologies 
+#Indentation is the space used to show hierarchy in control structures'
+#Statements are the instructions that are given to the computor program 
+
+
+#Print
+print("Teach me the print statment")
+print('you can use singel commas \n And use the slash n to put in to different line')
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+print(0 + 2, "You can do this too")  
+
+print("--" * 20)
+
+#Variables
+var = "luv"
+print("i", var, "python")
+
+var1 = 2
+var2 = 4
+print(var1 + var2)
+
+print("--" * 20)
+
+#Input
+storage = input("Whatever:")
+number_storage = int(input("Type a integer:"))
+print(type(number_storage))
+
+print("--" * 20)
+
+#Data Types
+a = 2
+b = "Annas"
+c = True 
+d = 3.14
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+
+print("--" * 20)
+
+#Lists
+example_list = ["Muhammad", "Ibrahim"]
+a, b = example_list
+print(a + b); print(a, b)
+
+
