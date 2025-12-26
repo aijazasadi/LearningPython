632958eaaab600efb1a9c3dabf9fa2c0f5c79c94
Asad Aijaz Arain <aijaz.asadi@gmail.com>
2025-12-26 14:55:29 +0500

    Update Glub-Fub-Robot.py
    updated random number code

--- Ibrahim/Glub_Fub_Robot.py
+++ Ibrahim/Glub_Fub_Robot.py
@@ -1,9 +1,15 @@
 #This robot will give you 1 of many random automated responses like 
 import random
 
-GlubFub_Storage = input("What do you want to ask Glub Fub:")
+GlubFub_Storage = input("What do you want to ask Glub Fub(1-5):")
+#Pascal Case but the second word starts with capital letter like Random_Gen
+#camel case like randomGen
+#snake case like random_gen
+first_random_number = int(input("Enter a first number:"))
+second_random_number = int(input("Enter a second number:"))
 
-Random_gen = random.randrange(1, 5)
+Random_gen = random.randrange(1, 5) 
+random_number = random.randint(first_random_number, second_random_number)
 
 if Random_gen == 1:
     print("Yes")
@@ -19,3 +25,11 @@ if Random_gen == 4:
 
 if Random_gen == 5:
     print("Never")
+
+# Asad's random code
+# you can use if-elif-else statement too
+# or you can use match-case statement
+if random_number % 2 == 0:
+    print(f"The random number is {random_number}, which is an even number.")
+else:  
+    print(f"The random number is {random_number}, which is an odd number.")
